498187a9bfb3ef7a29a908e4014aa87f1b16ac04
Jatin <jatin@live.ca>
2019-06-14 21:41:49 -0400

    Custom array adapter class implemented successfully. Tested successfully on phone.

---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,45 @@
+package com.example.android.miwok;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class WordAdapter extends ArrayAdapter<Word> {
+    public WordAdapter(@NonNull Context context, int resource, ArrayList<Word> words) {
+        super(context, resource, words);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // Get the {@link list_item} object located at this position in the list
+        Word currentWord = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID miwok_text_view
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        // Get the Miwok word from the current Word object and
+        // set this text on the name TextView
+        miwokTextView.setText(currentWord.getMiwokTranslation());
+
+        // Find the TextView in the list_item.xml layout with the ID english_text_view
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.english_text_view);
+        // Get the version number from the current Word object and
+        // set this text on the number TextView
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+        return listItemView;
+    }
+}
